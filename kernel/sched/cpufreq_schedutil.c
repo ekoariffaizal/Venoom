de78738cdb1ed3c3c0fa8a56fe7f10bd51833c3f
Parvinder Singh <psndna88@gmail.com>
2018-08-20 08:42:57 +0530

    disable tracing/logging at various places Part 2
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -136,8 +136,9 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		return;
 	}
 
-	sg_policy->next_freq = next_freq;
-	sg_policy->last_freq_update_time = time;
+	policy->cur = next_freq;
+//	trace_cpu_frequency(next_freq, smp_processor_id());
+}
 
 	if (policy->fast_switch_enabled) {
 		next_freq = cpufreq_driver_fast_switch(policy, next_freq);
