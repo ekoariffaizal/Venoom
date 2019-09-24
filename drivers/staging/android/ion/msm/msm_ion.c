7340ba93eb1b1d1a1a824a50c4734c90a4a5ce9a
Pzqqt <821026875@qq.com>
2019-09-24 12:04:53 +0800

    msm_ion.c, adreno_dispatch.c: fix build

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -305,10 +305,8 @@ static int ion_pages_cache_ops(struct ion_client *client,
 	int i;
 	unsigned int len = 0;
 	void (*op)(const void *, const void *);
-	struct ion_buffer *buffer;
 
-	buffer = get_buffer(handle);
-	table = buffer->sg_table;
+	table = ion_sg_table(client, handle);
 	if (IS_ERR_OR_NULL(table))
 		return PTR_ERR(table);
 
@@ -376,7 +374,7 @@ int ion_do_cache_op(struct ion_client *client, struct ion_handle *handle,
 	if (get_secure_vmid(flags) > 0)
 		return 0;
 
-	table = buffer->sg_table;
+	table = ion_sg_table(client, handle);
 
 	if (IS_ERR_OR_NULL(table))
 		return PTR_ERR(table);
