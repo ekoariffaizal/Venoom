58bca2a053a1f3524f3fa05d3aa9aeac09a039d8
Michael Ellerman <mpe@ellerman.id.au>
2019-11-13 21:05:44 +1100

    KVM: PPC: Book3S HV: Flush link stack on guest exit to host kernel
    commit af2e8c68b9c5403f77096969c516f742f5bb29e0 upstream.
    
    On some systems that are vulnerable to Spectre v2, it is up to
    software to flush the link stack (return address stack), in order to
    protect against Spectre-RSB.
    
    When exiting from a guest we do some house keeping and then
    potentially exit to C code which is several stack frames deep in the
    host kernel. We will then execute a series of returns without
    preceeding calls, opening up the possiblity that the guest could have
    poisoned the link stack, and direct speculative execution of the host
    to a gadget of some sort.
    
    To prevent this we add a flush of the link stack on exit from a guest.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    [dja: backport to v4.4, drop P9 support]
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/powerpc/include/asm/asm-prototypes.h
+++ arch/powerpc/include/asm/asm-prototypes.h
@@ -16,6 +16,7 @@
 extern s32 patch__call_flush_count_cache;
 extern s32 patch__flush_count_cache_return;
 extern s32 patch__flush_link_stack_return;
+extern s32 patch__call_kvm_flush_link_stack;
 extern long flush_count_cache;
 extern long kvm_flush_link_stack;
 
