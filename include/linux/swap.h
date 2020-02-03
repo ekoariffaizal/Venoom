e4261f7cfc816c6b8e181315ea31c5374cf8d00f
psndna88 <psndna88@gmail.com>
2020-02-03 19:16:51 +0530

    AGNi memory monitor: dynamic swappiness for zram v1.2
    zram swapping is allowed only when-
    1: available ram is less then 25% of total ram (auto detected)
    2: gpu load is less than 75% (no swapping while gaming, use those cpu cycles for game instead)
    3: battery is more than 25%
    
    Swappiness values: 1 or 30
    Trigger mechanism integrated in battery driver and calculations done on increase/decrease of battery %
    
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -11,6 +11,7 @@
 #include <linux/fs.h>
 #include <linux/atomic.h>
 #include <linux/page-flags.h>
+#include <linux/agni_meminfo.h>
 #include <asm/page.h>
 
 struct notifier_block;
@@ -371,12 +372,9 @@ static inline int mem_cgroup_swappiness(struct mem_cgroup *memcg)
 {
 	/* root ? */
 	if (mem_cgroup_disabled() || !memcg->css.parent) {
-		if (low_batt_swap_stall) {
+		if (low_batt_swap_stall || !triggerswapping) {
 			vm_swappiness = low_batt_swappiness;
 		} else {
-			vm_swappiness = agni_swappiness;
-		}
-		if (vm_swappiness > agni_swappiness) {
 			vm_swappiness = agni_swappiness;
 		}
 
@@ -389,16 +387,11 @@ static inline int mem_cgroup_swappiness(struct mem_cgroup *memcg)
 #else
 static inline int mem_cgroup_swappiness(struct mem_cgroup *mem)
 {
-	if (low_batt_swap_stall) {
+	if (low_batt_swap_stall || !triggerswapping) {
 		vm_swappiness = low_batt_swappiness;
 	} else {
 		vm_swappiness = agni_swappiness;
 	}
-
-	if (vm_swappiness > agni_swappiness) {
-		vm_swappiness = agni_swappiness;
-	}
-
 	return vm_swappiness;
 }
 #endif
