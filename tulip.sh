834d0341c89f533cb8ebf1d7716d465551b4c326
root <venoomtulip@gmail.com>
2020-03-28 18:49:53 +0000

    remove warning

--- tulip.sh
+++ tulip.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+\#!/bin/bash
 kernel_dir=$PWD
 export V="v1.5"
 export CONFIG_FILE="Ven_defconfig"
@@ -11,13 +11,13 @@ export CXX="ccache g++"
 export CCACHE="$(command -v ccache)"
 export CCACHE_DIR="~/.ccache"
 export "PATH=/usr/lib/ccache:$PATH"
-export CROSS_COMPILE_ARM32="/home/venoom/gcc/32/bin/arm-linux-androideabi-"
-export STRIP_KO="/mnt/d/Project/compiler/aarch64-linux-android/aarch64-unknown-linux-android/bin/strip"
+export CROSS_COMPILE_ARM32="/home/venoom/gcc/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+#export STRIP_KO="/mnt/d/Project/compiler/aarch64-linux-android/aarch64-unknown-linux-android/bin/strip"
 #export TOOL_CHAIN_PATH="/mnt/d/Project/compiler/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-export TOOL_CHAIN_PATH="/home/venoom/gcc/64/bin/aarch64-linux-android-"
+export TOOL_CHAIN_PATH="/home/venoom/gcc/gnu/bin/aarch64-linux-gnu-"
 #export CLANG_TCHAIN="/home/venoom/gcc/clang/bin/clang"
 export CLANG_TCHAIN="/home/venoom/gcc/clang/bin/clang"
-export CC="/home/venoom/gcc/bin/clang"
+export CC="/home/venoom/gcc/clang/bin/clang"
 export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | cut -d'(' -f1,4)"
 #export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
 #export PATH=$PATH:${TOOL_CHAIN_PATH}
@@ -40,7 +40,7 @@ compile() {
 	CC="${CCACHE} ${CC}" \
 	CLANG_TRIPLE=aarch64-linux-gnu- \
 	CROSS_COMPILE=${TOOL_CHAIN_PATH} \
-    KBUILD_COMPILER_STRING="${CLANG_VERSION}" \
+	KBUILD_COMPILER_STRING="${CLANG_VERSION}" \
 	HOSTCC="${CLANG_TCHAIN}" \
 	CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"\
     $JOBS
